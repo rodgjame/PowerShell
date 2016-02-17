ae5e431276622b1421843296232dc28466b57318
James William Rodgers <rodgjame@isu.edu>
2016-02-16 19:40:33 -0800

    Initial FizzBuzz commit

---
+++ FizzBuzz/FizzBuzz/fizzbuzz.ps1
@@ -0,0 +1,14 @@
+﻿#
+# Simple FizzBuzz Game
+#
+Param(
+[int]$gameLength
+)
+
+for($i = 1; $i -le $gameLength; $i++)
+{
+	if($i % 15 -eq 0) {echo "FizzBuzz"}
+	elseif($i % 5 -eq 0) {echo "Buzz"}
+	elseif($i % 3 -eq 0) {echo "Fizz"}
+	else {echo $i}
+}
